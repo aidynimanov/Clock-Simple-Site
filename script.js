dcf388d440edda0efa3c9c30686e2d5342f4c976
aidynimanov <aidyn.imanov@gmail.com>
2022-06-08 02:21:20 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+function showTime () {
+    let getClock = document.querySelector('.clock')
+    let date = new Date(); 
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let amPm = 'AM';
+
+    if (h==0) {
+        h == 12; 
+    }
+    if (h>12) {
+        h = h - 12;
+        amPm = 'PM'
+    }
+    
+    h = (h<10)? '0'+ h:m;
+    m = m<10? '0' + m : m;
+    s = s<10? '0' + s : s;
+
+    let time = h + ':'+ m + ':' + s + ' ' + amPm;
+getClock.textContent = time
+
+setTimeout(showTime,1000);
+
+}
+
+showTime();
